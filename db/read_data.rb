71bbb84bdb285e0a8030afa81e7138a0ca740122
JanTrooper2 <JanGoergens@gmail.com>
2020-11-08 15:39:38 +0100

    added seeding

---
+++ db/read_data.rb
@@ -0,0 +1,65 @@
+File.open("/home/jantrooper2/Projects/BayesianPredictions/db/scotts_predictions.txt", "r") do |f|
+  title = ["US", "ECONOMICS AND TECHNOLOGY", "CULTURE WARS", "COMMUNITIES", "PERSONAL", "SECRET"]
+  descriptions = []
+  @percentages = []
+  outcomes = []
+  validLinesArr = []
+  availableNumbers = (1..100).to_a 
+  def check_percent(line)
+    if line.include? "95"
+      @percentages.push(95)
+    elsif line.include? "90"
+      @percentages.push(90)
+    elsif line.include? "80"
+      @percentages.push(80)
+    elsif line.include? "70"
+      @percentages.push(70)
+    elsif line.include? "60"
+      @percentages.push(60)
+    elsif line.include? "50"
+      @percentages.push(50)
+    else
+      return false
+    end
+  end
+  f.each_line do |line|
+    line = line.strip
+
+    if line.include? "<p>"
+      next
+    elsif line.include? "<s>"
+      outcomes.push(false)
+      check_percent(line)
+      descriptions.push(line.slice(6..-14).strip)
+    else
+      outcomes.push(true)
+      check_percent(line)
+      descriptions.push(line.slice(3..-10).strip)
+    end
+  end
+
+  i = 0
+  descriptions.length.times do
+    puts (i+1).to_s + ". I predicted that: " + descriptions[i] + " comes true with a probability of: " + @percentages[i].to_s + "%, and it was " + outcomes[i].to_s
+    i += 1
+  end
+
+end
+# availableNumbers.each do |number| 
+#   if line[0, 5].include? number.to_s
+#     validLinesArr.push(line)
+#     if check_percent(line) == false
+#       puts line
+#     end
+#     break
+#   end
+# end
+
+#puts outcomes.length
+#puts @percentages.length
+#puts descriptions
+#puts validLinesArr.length
+#p outcomes
+#p @percentages
+#puts outcomes[65]
+#puts @percentages[65]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,96 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Prediction.create(name: "US Presidency", description: "Donald Trump remains president at end of year", probability_in_percent: 95, outcome: true, user_id: 1, expiration_date: "2018-12-31")
+
+def create_prediction(title, description, probability, outcome)
+  Prediction.create(name: title, description: description, probability_in_percent: probability, outcome: outcome, user_id: User.find_by(email: "developer@testing.com").id, expiration_date: "2018-12-31")
+end
+
+
+def open_and_read_file
+  File.open("/home/jantrooper2/Projects/BayesianPredictions/db/scotts_predictions.txt", "r") do |f|
+    descriptions = []
+    @percentages = []
+    outcomes = []
+    validLinesArr = []
+    availableNumbers = (1..100).to_a 
+    def check_percent(line)
+      if line.include? "95"
+        @percentages.push(95)
+      elsif line.include? "90"
+        @percentages.push(90)
+      elsif line.include? "80"
+        @percentages.push(80)
+      elsif line.include? "70"
+        @percentages.push(70)
+      elsif line.include? "60"
+        @percentages.push(60)
+      elsif line.include? "50"
+        @percentages.push(50)
+      else
+        return false
+      end
+    end
+    f.each_line do |line|
+      line = line.strip
+  
+      if line.include? "<p>"
+        next
+      elsif line.include? "<s>"
+        outcomes.push(false)
+        check_percent(line)
+        descriptions.push(line.slice(6..-14).strip)
+      else
+        outcomes.push(true)
+        check_percent(line)
+        descriptions.push(line.slice(3..-10).strip)
+      end
+    end
+    return descriptions, @percentages, outcomes
+  end
+end
+
+values = open_and_read_file()
+@title = ["US", "ECONOMICS AND TECHNOLOGY", "CULTURE WARS", "COMMUNITIES", "PERSONAL", "SECRET"]
+@descriptions = values[0]
+@probability = values[1]
+@outcome = values[2]
+#puts @title[0], @descriptions[0], @probability[0], @outcome[0]
+i = 0
+# US
+14.times do 
+  create_prediction(@title[0], @descriptions[i], @probability[i], @outcome[i])
+  i += 1
+end
+
+# Economics and Technology
+12.times do 
+  create_prediction(@title[1], @descriptions[i], @probability[i], @outcome[i])
+  i += 1
+end
+
+# Culture Wars
+2.times do 
+  create_prediction(@title[2], @descriptions[i], @probability[i], @outcome[i])
+  i += 1
+end
+
+# Communities
+17.times do 
+  create_prediction(@title[3], @descriptions[i], @probability[i], @outcome[i])
+  i += 1
+end
+
+# Personal
+18.times do 
+  create_prediction(@title[4], @descriptions[i], @probability[i], @outcome[i])
+  i += 1
+end
+
+# Secret
+36.times do 
+  create_prediction(@title[5], @descriptions[i], @probability[i], @outcome[i])
+  i += 1
+end
