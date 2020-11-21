632a1b246b03dd88c71e9c07b147414a0fe7034d
JanTrooper2 <JanGoergens@gmail.com>
2020-11-21 14:30:23 +0100

    added more user actions and file storage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
+  get 'profile/:id' => 'users#show'
+  get 'users' => 'users#index'
+  devise_for :users, controllers: { 
+    registrations: 'user/registrations',
+    sessions: 'devise/sessions',
+    passwords:'devise/passwords', 
+    confirmations: 'devise/confirmations', 
+    unlocks: 'devise/unlocks' 
+  }
   resources :predictions 
   get 'predictions_filtered', to: 'predictions#filter'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
